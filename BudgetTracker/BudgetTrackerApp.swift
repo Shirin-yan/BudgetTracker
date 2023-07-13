332ffac3566428c7352525a8dc649120f2ce966e
Shirin-yan <sirinangibaeva@Sirins-Laptop.local>
2023-07-13 10:00:49 +0500

    Created project

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Ширин Янгибаева on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
